af34ed3998fc101b19fe463a1cf4191323cab592
hansrwindhoff <hansrwindhoff@gmail.com>
2015-05-09 23:49:03 -0600

    cm

--- DDDNodeTS/AAAStartInDebugger.sh
+++ DDDNodeTS/AAAStartInDebugger.sh
@@ -2,7 +2,7 @@
 #xterm -e "node --debug-brk app.js" &
 xterm -e "node RemoteDebug.js app.js" &
 
-xterm -e "node-inspector --web-port=8090" &
+#xterm -e "node-inspector --web-port=8090" &
 
 
 #unused
