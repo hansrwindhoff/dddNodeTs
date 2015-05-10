cbdf3b6ebed77973e0bb7d507b1190e8e9d846ab
hansrwindhoff <hansrwindhoff@gmail.com>
2015-05-09 23:57:04 -0600

    rm

--- DDDNodeTS/AAAStartInDebugger.sh
+++ DDDNodeTS/AAAStartInDebugger.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-#xterm -e "node --debug-brk app.js" &
+
+
+#to attach with visual studio ntvs
 xterm -e "node RemoteDebug.js app.js" &
 
+
+#node inspector
+#xterm -e "node --debug-brk app.js" &
 #xterm -e "node-inspector --web-port=8090" &
 
 
