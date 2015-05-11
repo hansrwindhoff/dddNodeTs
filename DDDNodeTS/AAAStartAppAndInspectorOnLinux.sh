0c54a636e4fca547da9eafdb556f3999b34412e0
hansrwindhoff <hansrwindhoff@gmail.com>
2015-05-11 11:46:31 -0600

    sh

---
+++ DDDNodeTS/AAAStartAppAndInspectorOnLinux.sh
@@ -0,0 +1,3 @@
+xterm -e "sudo node --debug-brk app.js" &
+xterm -e "sudo node ./node_modules/node-inspector/bin/inspector.js" &
+
---
+++ DDDNodeTS/AAAStartInDebuggerLinux.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+#to attach with visual studio ntvs
+xterm -e "sudo node RemoteDebug.js app.js" &
+xterm -e "sudo node RemoteDebug.js app.js" &
+
+
+#node inspector
+#xterm -e "node --debug-brk app.js" &
+#xterm -e "node-inspector --web-port=8090" &
+
+
+#unused
