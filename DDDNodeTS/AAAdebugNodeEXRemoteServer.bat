1756cde203d0ea872c4b19ed3f33b2def947e777
hansrwindhoff <hansrwindhoff@gmail.com>
2015-05-11 11:43:37 -0600

    bat

--- DDDNodeTS/AAAdebugNodeEXRemoteServer.bat
+++ DDDNodeTS/AAAdebugNodeEXRemoteServer.bat
@@ -1,9 +1,8 @@
 rem by starting the node process inside a cmd /k shell the shell will stay open
 rem after the process finishes allowing to read any output
-
-
 rem start cmd /k  "node --debug-brk ex.js"
-
 rem start "" /D"." node-inspector --web-port=8090
-start chrome /new-window http://192.128.28.101:8090/debug?ws=192.128.28.101:8090&port=5858
-start chrome /new-window http://192.128.28.101:3000
+
+
+start chrome /new-window http://192.128.28.102:8080/debug?ws=192.128.28.102:8080&port=5858
+start chrome /new-window http://192.128.28.102:3000
